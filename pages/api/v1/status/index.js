43c4223db6e327f7f033f7bf5b51474e63cd1ea1
EngSoftSamuelSantos <125834503+EngSoftSamuelSantos@users.noreply.github.com>
2025-03-05 02:31:32 +0000

    criando váriaveis de ambiente `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const rresult = await database.query("SELECT 1 + 1 as sum;");
+  console.log(rresult.rows);
   response.status(200).json({ chave: "Samuel é acima da média" });
 }
 
