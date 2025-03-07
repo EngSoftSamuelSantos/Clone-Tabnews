d6c39ccd2f09d8e60ffff7f72db4470a178cf67e
EngSoftSamuelSantos <125834503+EngSoftSamuelSantos@users.noreply.github.com>
2025-03-07 02:08:18 +0000

    add ,

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,25 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const rresult = await database.query("SELECT 1 + 1 as sum;");
-  console.log(rresult.rows);
-  response.status(200).json({ chave: "Samuel é acima da média" });
+  const versionDbResult = await database.query("SHOW server_version;");
+  const versionDbValue = versionDbResult.rows[0].server_version;
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
+  const usedConnectionsResult = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  const usedConnectionsValue = usedConnectionsResult.rows[0].count;
+  console.log(usedConnectionsResult);
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      version: versionDbValue,
+      max_connections: parseInt(maxConnectionsValue),
+      opened_connections: usedConnectionsValue,
+    },
+    //used_connections: usedConnections.rowCount,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to /api/v1/status mostra retorno 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.dependencies.version).toEqual("16.8");
+  expect(responseBody.dependencies.max_connections).toEqual(100);
+  expect(responseBody.dependencies.opened_connections).toEqual(2);
 });
