d04dd61ea0e98e16a736b4610be10e67ea1266e4
EngSoftSamuelSantos <125834503+EngSoftSamuelSantos@users.noreply.github.com>
2025-03-09 23:06:04 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,24 +1,34 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
-  const versionDbResult = await database.query("SHOW server_version;");
-  const versionDbValue = versionDbResult.rows[0].server_version;
-  const maxConnectionsResult = await database.query("SHOW max_connections;");
-  const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
-  const usedConnectionsResult = await database.query(
-    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
-  );
-  const usedConnectionsValue = usedConnectionsResult.rows[0].count;
-  console.log(usedConnectionsResult);
   const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
-      version: versionDbValue,
-      max_connections: parseInt(maxConnectionsValue),
-      opened_connections: usedConnectionsValue,
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
     },
-    //used_connections: usedConnections.rowCount,
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,11 +3,11 @@ test("GET to /api/v1/status mostra retorno 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-  expect(responseBody.dependencies.version).toEqual("16.8");
-  expect(responseBody.dependencies.max_connections).toEqual(100);
-  expect(responseBody.dependencies.opened_connections).toEqual(2);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.8");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
